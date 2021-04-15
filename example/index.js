7e219b51bc97457072f7bd59981c21e86f1f5240
Px <>
2021-04-15 17:54:59 +0200

    Removed decimals from token list

--- example/index.js
+++ example/index.js
@@ -2,9 +2,9 @@ const wallet = require('./../queries/wallet');
 
 const main = async () => {
 	//array of tokens contracts to call
-	const tokens = [{ address: '0x3050e20fabe19f8576865811c9f28e85b96fa4f9', decimals: 18 }];
+	const tokens = ['0x3050e20fabe19f8576865811c9f28e85b96fa4f9'];
 	const tokenBalances = await wallet.simplyTokenBalances({
-		user_address: '0x...',
+		user_address: '0x3816CC15C628c1647EB39e6B3C3c02ee9B770741',
 		network: 'rinkeby',
 		tokens: tokens,
 	});
