a11903ceb8f9e4da7ee9db82408443341a236805
Px <>
2021-04-15 18:10:12 +0200

    Update example

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const main = async () => {
 	//array of tokens contracts to call
 	const tokens = ['0x3050e20fabe19f8576865811c9f28e85b96fa4f9'];
 	const tokenBalances = await wallet.simplyTokenBalances({
-		user_address: '0x3816CC15C628c1647EB39e6B3C3c02ee9B770741',
+		user_address: '0x00',
 		network: 'rinkeby',
 		tokens: tokens,
 	});
